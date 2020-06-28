4cc2de913805f45f6f6205d35adbfc6227e936c6
cmorg040 <33102191+cmorg040@users.noreply.github.com>
2020-06-28 12:48:00 -0400

    Forked latest version
    Forking latest version of Services-Status for easier pull requests.

---
+++ AMPATH/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\monk\ampath\ampath_test\services-status\ampath\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
