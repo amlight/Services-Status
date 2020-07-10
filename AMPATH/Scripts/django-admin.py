8f6adeb4b6a62598c9673bd77dba8ed7cf80e032
cmorg040 <33102191+cmorg040@users.noreply.github.com>
2020-07-10 12:15:02 -0400

    Removed Virtual Environment files

--- AMPATH/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\monk\ampath\ampath_test\services-status\ampath\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
